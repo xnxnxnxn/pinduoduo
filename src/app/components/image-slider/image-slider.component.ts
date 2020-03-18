2438e4ede47fa43a81004576c2b54cc5237f3460
xnxnxnxn <574431874@qq/com>
2020-03-18 21:56:04 +0800

    轮播图实现

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, OnDestroy, Renderer2, ViewChild, ElementRef } from '@angular/core';
 
 export interface ImageSlider {
   imgUrl: string;
@@ -11,10 +11,58 @@ export interface ImageSlider {
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.less']
 })
-export class ImageSliderComponent implements OnInit {
+export class ImageSliderComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input()
   sliders: ImageSlider[] = [];
-  constructor() { }
+  // 图片高度
+  @Input()
+  sliderHeight = 160;
+  // 播放间隔时间单位 s
+  @Input() 
+  intervalBySeconds = 2;
+
+  @ViewChild('imageSlider', { static: true })
+  imgSlider: ElementRef;
+  // 定时播放
+  intervalId;
+  // 当前滚动下标
+  selectedIndex = 0;
+
+  constructor(private rd2: Renderer2) { }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+
+  ngAfterViewInit(): void {
+    if (this.intervalBySeconds <= 0) {
+      return;
+    }
+    this.intervalId = setInterval(() => {
+        // 使用rd2让组件向左滑动
+        this.rd2.setProperty(
+          this.imgSlider.nativeElement,
+          'scrollLeft',
+          (this.getIndex(++this.selectedIndex) *
+          this.imgSlider.nativeElement.scrollWidth) /
+          this.sliders.length
+        );
+    }, this.intervalBySeconds * 1000);
+  }
+
+  getIndex(idx: number): number {
+    console.log(idx);
+    return idx >= 0
+      ? idx % this.sliders.length
+      : this.sliders.length - (Math.abs(idx) % this.sliders.length);
+  }
+
+  handleScroll(ev) {
+    const ratio =
+      ev.target.scrollLeft / (ev.target.scrollWidth / this.sliders.length);
+    this.selectedIndex = Math.round(ratio);
+  }
 
   ngOnInit() {
   }
